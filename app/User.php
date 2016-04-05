cf69d14f72326f426edda2845bc33e17f2b6dd56
reneedudley <4reneedudley@gmail.com>
2016-04-05 12:27:27 -0500

    for real initial

--- app/User.php
+++ app/User.php
@@ -16,40 +16,11 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes excluded from the model's JSON form.
+     * The attributes that should be hidden for arrays.
      *
      * @var array
      */
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    /**
-     * Get the subbreddits for the user.
-     */
-    public function subbreddits()
-    {
-        return $this->hasMany('App\Subbreddit');
-    }
-    /**
-     * Get the posts for the user.
-     */
-    public function posts()
-    {
-        return $this->hasMany('App\Post');
-    }
-     /**
-     * Get the subscribed subbreddits for the user.
-     */
-    public function subscribedSubbreddits()
-    {
-        return $this->belongsToMany('App\Subbreddit ');
-    }
-    /**
-     * Get the comments for the user.
-     */
-    public function comments()
-    {
-        return $this->hasMany('App\Comment');
-    }
 }
